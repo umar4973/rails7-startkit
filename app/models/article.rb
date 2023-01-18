4f27804923c09e08bb96fef1a803426ce4e440c4
Ilya N. Zykin <zykin-ilya@ya.ru>
2023-01-19 00:15:55 +0300

    Add Whenever

--- app/models/article.rb
+++ app/models/article.rb
@@ -13,6 +13,16 @@ class Article < ApplicationRecord
   # Kaminari. Pagination
   paginates_per 3
 
+  # rails runner Article.get_currency
+  def self.get_currency
+    url = "https://api.exchangerate.host/latest?base=USD&symbols=KGS"
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    response_obj = JSON.parse(response)
+    puts "[#{DateTime.now.strftime('%d.%m.%Y %H.%M.%S')}]: " + \
+         "USD:KGS currency rate: " + response_obj['rates']['KGS'].to_s
+  end
+
   # Take a raw content from a user and save it in `content_raw`.
   # But never render `content_raw` on a page. It is dangerous!
   # Process a content once `on save` and cut off dangerous tags.
